156815fe392e4f5c8f1ab235ee23f1ade6b6ee9a
dorvillethiago <dorville.dev@gmail.com>
2025-03-28 16:45:36 -0300

    refactor testimonials section for improved layout and responsiveness; add mainDescription to testimonial data

--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -2,49 +2,39 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Card, CardContent } from '@/components/ui/card'
 import { type Testimonial, testimonials } from '@/static'
 
-const chunkArray = (array: Testimonial[], chunkSize: number): Testimonial[][] => {
-    const result: Testimonial[][] = []
-    for (let i = 0; i < array.length; i += chunkSize) {
-        result.push(array.slice(i, i + chunkSize))
-    }
-    return result
-}
-
-const testimonialChunks = chunkArray(testimonials, Math.ceil(testimonials.length / 3))
-
 export default function WallOfLoveSection() {
     return (
         <section>
             <div className="py-16">
                 <div className="mx-auto max-w-6xl px-3 xl:px-0">
-                    <div className="text-center">
+                    <div className="text-center ">
                         <h2 className="text-title text-3xl font-semibold">A confiança dos nossos clientes!</h2>
-                        <p className="text-body mt-6">O que os nossos clientes tem dito sobre nós</p>
+                        <p className="text-body mt-4">O que os nossos clientes tem dito sobre nós</p>
                     </div>
-                    <div className="mt-8 grid gap-3 sm:grid-cols-2 md:mt-12 lg:grid-cols-3">
-                        {testimonialChunks.map((chunk, index) => (
-                            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                            <div key={index} className="space-y-3">
-                                {chunk.map(({ name, role, quote, image }) => (
-                                    <Card key={name}>
-                                        <CardContent className="grid grid-cols-[auto_1fr] gap-3 pt-6">
-                                            <Avatar className="size-9">
-                                                <AvatarImage alt={name} src={image} loading="lazy" width="120" height="120" />
-                                                <AvatarFallback>ST</AvatarFallback>
-                                            </Avatar>
+                    <div className="mt-16 gap-18 lg:gap-4 md:mt-22 flex justify-center flex-wrap">
+                        {testimonials.map(({ name, role, quote, image, mainDescription }, index) => (
+                            <div key={index} className="space-y-3 drop-shadow-xl">
+                                <Card key={name}>
+                                    <CardContent className="flex flex-col gap-2 relative items-center justify-center text-center max-w-[370px] min-h-[100px] max-h-[250px]">
+                                        <Avatar className="size-20 absolute border -top-16">
+                                            <AvatarImage alt={name} src={image} loading="lazy" width="120" height="120" />
+                                            <AvatarFallback>ST</AvatarFallback>
+                                        </Avatar>
+                                        <div className='mt-10'>
+                                            <h2 className='font-medium'>{mainDescription}</h2>
 
-                                            <div>
-                                                <h3 className="font-medium">{name}</h3>
+                                            <blockquote className="mt-3">
+                                                <p className="text-gray-700 dark:text-gray-300">{quote}</p>
+                                            </blockquote>
+                                            <br />
 
-                                                <span className="text-muted-foreground block text-sm tracking-wide">{role}</span>
+                                            <h3 className="font-medium">{name}</h3>
 
-                                                <blockquote className="mt-3">
-                                                    <p className="text-gray-700 dark:text-gray-300">{quote}</p>
-                                                </blockquote>
-                                            </div>
-                                        </CardContent>
-                                    </Card>
-                                ))}
+                                            <span className="text-muted-foreground block text-sm tracking-wide">{role}</span>
+
+                                        </div>
+                                    </CardContent>
+                                </Card>
                             </div>
                         ))}
                     </div>
